bbb82e22aa73dc3608d56b3466e3c31414a07137
Troy <5659019+troyhacks@users.noreply.github.com>
2024-04-19 11:17:00 -0400

    Testing a build fix

--- wled00/network.cpp
+++ wled00/network.cpp
@@ -211,7 +211,7 @@ void WiFiEvent(WiFiEvent_t event) {
   DEBUG_PRINT(F(" = "));
 
   switch (event) {
-    #ifdef ARDUINO_EVENT_WIFI_STA_GOT_IP
+    #ifdef ESP32WIFIGENERIC_H_
     case ARDUINO_EVENT_WIFI_STA_GOT_IP:
       if (Network.isEthernet()) {
         if (!apActive) {
