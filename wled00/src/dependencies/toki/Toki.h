f035f30c0c8afe4f2a116b6e5bbc10fd7cb11804
Troy <5659019+troyhacks@users.noreply.github.com>
2024-07-21 08:35:21 -0400

    Fix minior sprintf warning in Toki

--- wled00/src/dependencies/toki/Toki.h
+++ wled00/src/dependencies/toki/Toki.h
@@ -156,6 +156,6 @@ class Toki {
     }
 
     void printTime(const Time& t) {
-      if (Serial) Serial.printf_P(PSTR("%u,%03u\n"),t.sec,t.ms);
+      if (Serial) Serial.printf_P(PSTR("%lu,%03u\n"),t.sec,t.ms);
     }
 };
