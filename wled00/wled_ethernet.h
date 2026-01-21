a540b9675d8d5549f36608991ca7af1d91338274
Troy <5659019+troyhacks@users.noreply.github.com>
2026-01-20 20:04:57 -0500

    This is not in the right place.  What place should it be in? I dunno!

--- wled00/wled_ethernet.h
+++ wled00/wled_ethernet.h
@@ -22,9 +22,9 @@
   #define GPIO_NUM_NC -1
 #endif
 
-#ifndef ETH_PHY_W5500
-  #define ETH_PHY_W5500 ETH_PHY_MAX 
-#endif
+// #ifndef ETH_PHY_W5500
+//   #define ETH_PHY_W5500 ETH_PHY_MAX 
+// #endif
 
 #ifdef CONFIG_ETH_PHY_INTERFACE_RMII
 typedef struct EthernetSettings {
