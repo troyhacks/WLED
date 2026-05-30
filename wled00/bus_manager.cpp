057b37b1c835346d1f85e5ec82082a7957d74274
Will <71540407+Will-wastelander@users.noreply.github.com>
2026-05-29 20:55:36 -0700

    Add support for Waveshare S3 Matrix Driver board
    https://www.waveshare.com/esp32-s3-rgb-matrix.htm
    https://docs.waveshare.com/ESP32-S3-RGB-Matrix

--- wled00/bus_manager.cpp
+++ wled00/bus_manager.cpp
@@ -803,6 +803,12 @@ BusHub75Matrix::BusHub75Matrix(BusConfig &bc) : Bus(bc.type, bc.start, bc.autoWh
   // HUB75_I2S_CFG::i2s_pins _pins={R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A_PIN, B_PIN, C_PIN, D_PIN, E_PIN, LAT_PIN, OE_PIN, CLK_PIN};
   mxconfig.gpio = { 1, 5, 6, 7, 13, 9, 16, 48, 47, 21, 38, 8, 4, 18 };
 
+#elif defined(WAVESHARE_S3_PINOUT)
+  DEBUGBUS_PRINTLN("MatrixPanel_I2S_DMA - Waveshare S3 with PSRAM, Waveshare pinout");
+
+  // HUB75_I2S_CFG::i2s_pins _pins={R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A_PIN, B_PIN, C_PIN, D_PIN, E_PIN, LAT_PIN, OE_PIN, CLK_PIN};
+  mxconfig.gpio = {4, 5, 6, 7, 15, 16, 18, 8, 3, 42, 9, 40, 2, 41};
+
   #else
   USER_PRINTLN("MatrixPanel_I2S_DMA - S3 with PSRAM");
 
