7c55f91f1aecca548a63582e25934fdb0b7f6988
Frank <91616163+softhack007@users.noreply.github.com>
2026-07-01 12:11:59 +0200

    build 2607011

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
 
 // version code in format yymmddb (b = daily build)
 #ifndef WLED_BUILD_VERSION // WLEDMM allow override by nightly build script
-  #define VERSION 2606201
+  #define VERSION 2607011
 #else
   #define VERSION WLED_BUILD_VERSION
 #endif
